9a526dd760873eeefe31e14e2a9bbbb4d68723d3
ingriun <ingridundeberget@gmail.com>
2025-01-16 16:50:01 +0100

    Update functions.cpp

--- functions.cpp
+++ functions.cpp
@@ -1,6 +1,16 @@
 #include <stdbool.h>
 #include <functions.h>
 
-int metropolis(int D, int N, float beta, float b, int seed, int N_config, bool config_type){}
+int metropolis(int D, int N, float beta, float b, int seed, int N_config, bool config_type_hot){
+    if(config_type_hot){
+        //call setup hot
+    } else {//call setup cold
+    }
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j < N; j++){
+            
+        }
+    }
+}
 
 int replica_method(int D, int N, float beta, float b, int N_config, bool config_type){}
