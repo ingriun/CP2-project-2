25a97b7b545cf22a77ee920ec6a9fc2e39379a23
ingriun <ingridundeberget@gmail.com>
2025-01-22 09:16:35 +0100

    function as parameter

--- functions.cpp
+++ functions.cpp
@@ -1,11 +1,8 @@
 #include <stdbool.h>
 #include <functions.h>
 
-int metropolis(int D, int N, float beta, float b, int seed, int N_config, bool config_type_hot){
-    if(config_type_hot){
-        //call setup hot
-    } else {//call setup cold
-    }
+int metropolis(int D, int N, float beta, float b, int seed, int N_config, function<vector<vector<int>>(int, int)> config_type){
+    vector<vector<int>> magnet = config_type(D, N);
     for(int i = 0; i < N; i++){
         for(int j = 0; j < N; j++){
             
--- functions.h
+++ functions.h
@@ -1,5 +1,5 @@
 #include <setup.h>
 
-int metropolis(int D, int N, float beta, float b, int seed, int N_config, bool config_type);
+int metropolis(int D, int N, float beta, float b, int seed, int N_config, function<vector<vector<int>>(int, int)> config_type);
 
 int replica_method(int D, int N, float beta, float b, int N_config, bool config_type);
