3a1df40447aff447d8cf8efbb16459753e8a7c49
ingriun <ingridundeberget@gmail.com>
2025-01-29 10:50:37 +0100

    Update functions.h

--- functions.h
+++ functions.h
@@ -1,6 +1,6 @@
 #include "setup.h"
 #include <tuple>
 
-tuple<vector<vector<int>>, double> metropolis(int D, int N, float beta, float b, int seed, int N_config, function<vector<vector<int>>(int, int)> config_type);
+tuple<vector<vector<int>>, double> metropolis(int D, int N, float beta, float b, int seed, int N_config, char config_type);
 
-int replica_method(int D, int N, float beta, float b, int N_config, function<vector<vector<int>>(int, int)> config_type);
+int replica_method(int D, int N, float beta, float b, int N_config, char config_type = 'h', int R = 500);
