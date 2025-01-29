a9ace5fddce117437070225449c0ad76fc8b0842
Max Simonnet <max.simonnet07@gmail.com>
2025-01-29 14:01:57 +0100

    updated script.sh

--- script.sh
+++ script.sh
@@ -4,6 +4,14 @@
 
 # Be sure to be in the right directory (CP2-project-2) !!!
 
+
+
+# if 'ising' not present in the project, write either :
+
+# Linux : g++ main.cpp setup.cpp functions.cpp -o ising
+# Mac : clang++ main.cpp setup.cpp functions.cpp -o ising
+
+
 D=2
 N=100
 beta=0.43
