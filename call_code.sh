0f2c3f5728298eebec9efa5f281c07e6b965c927
Max Simonnet <max.simonnet07@gmail.com>
2025-01-30 15:59:27 +0100

    changed scripts

---
+++ call_code.sh
@@ -0,0 +1,16 @@
+
+#! /bin/bash
+
+# Usage in terminal : bash call_code.sh
+
+# Be sure to be in the right directory (CP2-project-2) !!!
+
+D=2
+N=100
+beta=0.43
+b=0.01
+seed=28
+N_config=1000
+config_type='c'
+
+./ising $D $N $beta $b $seed $N_config $config_type
---
+++ mac_binary.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+# Usage in terminal : bash linux_binary.sh
+
+# Be sure to be in the right directory (CP2-project-2) !!!
+
+
+
+clang++ main.cpp setup.cpp functions.cpp -o ising
