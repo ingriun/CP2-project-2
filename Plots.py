33eedbe4f85c7d110f1bd76a68c2d732e76bdee9
chloelcon <me@chloelee.de>
2025-02-07 00:43:48 +0100

    splitting large file, plotting for different files

--- Plots.py
+++ Plots.py
@@ -1,11 +1,47 @@
 import matplotlib.pyplot as plt
+from matplotlib.ticker import MaxNLocator
 import numpy as np
+import pandas as pd
+
+"""def split_large_csv():
+    for i,chunk in enumerate(pd.read_csv('energy.csv', chunksize=501000)):
+        filename = f'energy_{i}.csv'
+        chunk.to_csv(filename, index=False)
+        print(f'created: {filename}')
+
+    for j,chunk in enumerate(pd.read_csv('magnetisation.csv', chunksize=501000)):
+        filename = f'magnetisation_{j}.csv' 
+        chunk.to_csv(filename, index=False)
+        print(f'created: {filename}')
 
 
-def thermalization():
+split_large_csv()"""
+
+
+
+def plot_data_subset(start_line: int=2, end_line: int=1002):
+    data = pd.read_csv('energy_27.csv').iloc[start_line:end_line+1]
+
+    plt.figure(figsize=(10,6))
+
+    for col in data.columns:
+        plt.plot(data[col].values, label='Energy')
+
+    plt.xlabel('Configuration')
+    plt.ylabel('Energy')
+    plt.title('Energy')
+
+    ax = plt.gca()
+    ax.yaxis.set_major_locator(MaxNLocator(nbins=10))  # Maximum 10 y-ticks
+
+    plt.show()
+
+plot_data_subset(start_line=2, end_line=1002)
+
+"""def thermalization():
 
     # Value of magnetization for each config
-    mag = np.loadtxt("magnetisation.csv")
+    mag = np.loadtxt("magnetisationdata.csv")
 
     # indice of the current config
     indice = np.arange(1,len(mag)+1)
@@ -17,7 +53,7 @@ def thermalization():
     ax.set_xlabel("Indice")
     ax.set_ylabel("Magnetization M")
 
-    energy = np.loadtxt("energy.csv")
+    energy = np.loadtxt("energydata.csv")
 
     fig, ax1 = plt.subplots()
 
@@ -53,7 +89,7 @@ def magnetization_plot():
 def average():
 
     mag_all = 0
-    mag_array = np.loadtxt("magnetisation.csv")
+    mag_array = np.loadtxt("magnetisationdata.csv")
 
     for i in range(150,1000):
         mag_all += mag_array[i]
@@ -61,7 +97,7 @@ def average():
     mean_mag = 1/(1000 - 150)*mag_all
 
     energy_all = 0
-    energy_array = np.loadtxt("energy.csv")
+    energy_array = np.loadtxt("energydata.csv")
 
     for i in range(150,1000):
         energy_all += energy_array[i]
@@ -71,8 +107,8 @@ def average():
     return mean_mag, mean_energy 
 
 
-thermalization()
+#thermalization()
 
 mean_mag,mean_energy = average()
 print("Mean magnetisation : ", mean_mag)
-print("Mean energy : ", mean_energy)
+print("Mean energy : ", mean_energy)"""
